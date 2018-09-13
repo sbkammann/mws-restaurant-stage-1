8a852def93c1b3c66192236e2d1bd084e27b1988
Shannan Kammann <sbkammann@gmail.com>
2018-09-13 14:47:11 -0700

    fix: Change View Details link to button

--- js/main.js
+++ js/main.js
@@ -165,19 +165,23 @@ createRestaurantHTML = (restaurant) => {
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
+  name.setAttribute('tabindex', '0');
   li.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
+  neighborhood.setAttribute('tabindex', '0');
   li.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
+  address.setAttribute('tabindex', '0');
   li.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('button'); //button //a
   more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
+  more.type = 'button';
+  more.setAttribute( 'onclick', `location.href = '${DBHelper.urlForRestaurant(restaurant)}'`);
   li.append(more)
 
   return li
